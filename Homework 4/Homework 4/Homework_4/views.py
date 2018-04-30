d5b0f40d187b38242084c1f4868311bf043f76d8
Andrei Herdes <andrei.herdes@info.uaic.ro>
2018-04-30 22:49:01 +0300

    Working on populating the html table with db records

--- Homework 4/Homework 4/Homework_4/views.py
+++ Homework 4/Homework 4/Homework_4/views.py
@@ -3,7 +3,7 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template, request, json
+from flask import render_template, request, json, send_from_directory
 from Homework_4 import app
 import requests
 import json
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from io import BytesIO
 import pyodbc
+from bs4 import BeautifulSoup as Soup
 
 subscription_key = "683934df678c4864b7d5f3ddbffffa3a"
 assert subscription_key
@@ -70,3 +71,46 @@ def addInBd(url, description):
     cnxn.commit()
     cnxn.close()
     return json.dumps("OK")
+
+@app.route('/database')
+def getDatabase():
+    db_records = getImages()
+    html_text = '\n'.join(create_html_table(db_records))
+    html_records = Soup(html_text, "html.parser")
+    with open(r'E:\Facultate\an3-sem2\CloudComputing\repositories\Azure\Homework 4\Homework 4\Homework_4\templates\bd.html', 'r+') as fp:
+        soup = Soup(fp, "html.parser")
+    thead_tag = soup.find('thead')
+    thead_tag.insert_after(html_records)
+    return send_from_directory('html',
+                               r'E:\Facultate\an3-sem2\CloudComputing\repositories\Azure\Homework 4\Homework 4\Homework_4\templates\bd.html')
+
+
+def create_html_table(image_list):
+  yield ('<tbody id="imagesTbody">')
+  for image in image_list:
+    yield ('  <tr><td scope="col">' + image[0] + '</td>')
+    yield ('      <td scope="col">' + image[1] + '</td>')
+    yield ('      <td scope="col">')
+    yield ('          <button type="button" class="send_email" data-toggle="modal" data-target="#exampleModal">')
+    yield ('              Send Email')
+    yield ('          </button>')
+    yield ('      </td>')
+    yield ('  </tr>')
+  yield ('</tbody>')
+
+def getImages():
+    server = 'azurebidi.database.windows.net'
+    database = 'ImagesTornado'
+    username = 'AzureBidi'
+    password = 'Azure6598'
+    driver= '{ODBC Driver 13 for SQL Server}'
+
+    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT * FROM Image")
+
+    db_records = []
+    for row in cursor.fetchall():
+        db_records.append((row[1], row[2]))
+
+    return db_records
